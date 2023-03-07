8ea54a477d6335906ea93d67594b497ac54446a3
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-06 23:56:07 -0500

    Add tailwind and use it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () => {
+  const categories = [
+    { title: 'Hats', id: 1, image: 'https://i.ibb.co/cvpntL1/hats.png' },
+    { title: 'Jackets', id: 2, image: 'https://i.ibb.co/px2tCc3/jackets.png' },
+    {
+      title: 'Sneakers',
+      id: 3,
+      image: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    { title: 'Women', id: 4, image: 'https://i.ibb.co/GCCdy8t/womens.png' },
+    { title: 'Men', id: 5, image: 'https://i.ibb.co/R70vBrQ/men.png' },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app__categories-container'>
+      {categories.map(({ title, id, image }) => (
+        <div className='app__category-container' key={id}>
+          <div className='app__category-image'>
+            <img src={image} alt={title} border='0' />
+          </div>
+          <div className='app__category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
