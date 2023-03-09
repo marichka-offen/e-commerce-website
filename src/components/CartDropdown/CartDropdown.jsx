af37f0f0ceaa57d8e9512cf43be06c27f5944dfd
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-09 11:28:16 -0500

    Build out cart dropdown

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/CartContext'
 import Button from '../Button/Button'
+import CartItem from '../CartItem/CartItem'
 import './CartDropdown.css'
 
-const CartDropdown = () => (
-  <div className='cart-dropdown'>
-    <div className='cart-dropdown__cart-items' />
-    <Button text='Go to checkout' />
-  </div>
-)
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext)
+  return (
+    <div className='cart-dropdown'>
+      <div className='cart-dropdown__cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <Button text='Go to checkout' />
+    </div>
+  )
+}
 
 export default CartDropdown
