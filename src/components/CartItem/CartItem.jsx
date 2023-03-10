870cb1c1632ae37bbc3fd50da2ed95a45380ac57
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-09 21:58:04 -0500

    Update cartItem with newly created components

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,13 +1,19 @@
 import './CartItem.css'
+import QuantitySelector from '../QuantitySelector/QuantitySelector'
+import RemoveProductButton from '../RemoveProductButton/RemoveProductButton'
 
-const CartItem = ({ cartItem: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({ cartItem }) => {
+  const { id, imageUrl, price, name } = cartItem
   return (
     <div className='cart-item'>
       <img src={imageUrl} alt={`${name}`} className='cart-item__item-image' />
       <div className='cart-item__item-details'>
-        <span className='cart-item__name'>{name}</span>
+        <div className='cart-item__item-details--top-row'>
+          <span className='cart-item__name'>{name}</span>
+          <RemoveProductButton id={id} small />
+        </div>
         <span className='cart-item__price'>
-          {quantity} x ${price}
+          <QuantitySelector product={cartItem} small /> &#215; ${price}
         </span>
       </div>
     </div>
